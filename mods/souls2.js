29eaeff8a70121eb7e2104bdba7147a3db9dd347
lol-109 <anotes537@gmail.com>
2025-07-07 11:34:53 -0500

    Update souls2.js

--- mods/souls2.js
+++ mods/souls2.js
@@ -127,7 +127,7 @@ elements.ectoplasm = {
         "body": { attr2:{"panic":20} },
         "rock_wall": { elem1:null, elem2:"tombstone" }
     },
-    temp: -10,
+    temp: 29,
     category: "liquids",
     state: "liquid",
     density: 0.0001,
